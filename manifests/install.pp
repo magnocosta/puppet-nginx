7cb3859ace3805ef3ce0f62df3b21d2e02817bf0
Magno Costa <magnocosta.br@gmail.com>
2015-09-22 18:55:55 -0300

    Create puppet nginx project

---
+++ manifests/install.pp
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+class nginx::install {
+
+  # Update the packages apt
+  exec { "apt-update-nginx":
+    command => "/usr/bin/apt-get update",
+  }
+
+  package {["nginx"]:
+    ensure  => installed,
+    require => Exec["apt-update-apache"]
+  }
+
+}
